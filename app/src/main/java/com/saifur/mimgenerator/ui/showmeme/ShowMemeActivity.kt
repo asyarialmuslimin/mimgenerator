a6515577d3993f1659a843892094249ad1da1796
Ahmad Saifur Ridlo <saifurridho810@gmail.com>
2021-01-11 12:42:36 +0700

    fix add text to image

--- app/src/main/java/com/saifur/mimgenerator/ui/showmeme/ShowMemeActivity.kt
+++ app/src/main/java/com/saifur/mimgenerator/ui/showmeme/ShowMemeActivity.kt
@@ -4,11 +4,12 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
-import android.view.DragEvent
-import android.view.View
+import android.view.*
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -16,7 +17,6 @@ import com.bumptech.glide.Glide
 import com.saifur.mimgenerator.R
 import com.saifur.mimgenerator.data.model.memeresponse.Meme
 import com.saifur.mimgenerator.databinding.ActivityShowMemeBinding
-import org.koin.android.ext.android.bind
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
@@ -24,6 +24,8 @@ import java.io.IOException
 
 class ShowMemeActivity : AppCompatActivity() {
     private lateinit var binding:ActivityShowMemeBinding
+
+    private var mIsScrolling = false
 
     private var textSize = 8
 
@@ -54,11 +56,34 @@ class ShowMemeActivity : AppCompatActivity() {
             val dialogLayout = inflater.inflate(R.layout.add_text_dialog, null)
             val editText = dialogLayout.findViewById<EditText>(R.id.et_text)
             val seekBar = dialogLayout.findViewById<SeekBar>(R.id.seekBar)
-            val seekBarValue = dialogLayout.findViewById<TextView>(R.id.seekbar_value)
+            val whiteButton = dialogLayout.findViewById<ImageButton>(R.id.btn_white)
+            val whiteDrawable = whiteButton.background as GradientDrawable
+            val blackButton = dialogLayout.findViewById<ImageButton>(R.id.btn_black)
+            val blackDrawable = blackButton.background as GradientDrawable
+            var textColor = Color.BLACK
+
+            editText.textSize = 20f
+            seekBar.progress = 20
+
+            whiteButton.setOnClickListener {
+                whiteDrawable.setStroke(2, Color.YELLOW)
+                blackDrawable.setStroke(2, Color.BLACK)
+                textColor = Color.WHITE
+                editText.setBackgroundColor(Color.BLACK)
+                editText.setTextColor(Color.WHITE)
+            }
+
+            blackButton.setOnClickListener {
+                whiteDrawable.setStroke(2, Color.BLACK)
+                blackDrawable.setStroke(2, Color.YELLOW)
+                textColor = Color.BLACK
+                editText.setBackgroundColor(Color.WHITE)
+                editText.setTextColor(Color.BLACK)
+            }
 
             seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                    seekBarValue.text = progress.toString()
+                    editText.textSize = progress.toFloat()
                     textSize = progress
                 }
 
@@ -78,29 +103,12 @@ class ShowMemeActivity : AppCompatActivity() {
                     val customText = TextView(this)
                     customText.textSize = textSize.toFloat()
                     customText.text = editText.text.toString()
-                    customText.setOnLongClickListener{ v ->
-                        val shadow = View.DragShadowBuilder(v)
-                        v.startDrag(null, shadow, v, 0)
-                        true
-                    }
+                    customText.setTextColor(textColor)
+                    val container = RelativeLayout(this)
+                    customText.setOnTouchListener(MoveViewTouchListener(container))
+                    container.addView(customText)
 
-                    customText.setOnDragListener { _, event ->
-                        Log.d("Drag", event.action.toString())
-                        if(event.action == DragEvent.ACTION_DRAG_EXITED){
-                            val x = event.x.toInt()
-                            val y = event.y.toInt()
-                            val layoutParam = RelativeLayout.LayoutParams(
-                                    RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT
-                            )
-
-                            layoutParam.setMargins(x, y, 0, 0)
-
-                            customText.layoutParams = layoutParam
-                        }
-                        true
-                    }
-
-                    binding.canvasLayout.addView(customText)
+                    binding.canvasLayout.addView(container)
                 }
             }
 
@@ -168,6 +176,50 @@ class ShowMemeActivity : AppCompatActivity() {
 
     companion object{
         const val EXTRA_DETAIL = "extra_detail"
+    }
+
+    inner class MoveViewTouchListener(iView: View) : View.OnTouchListener {
+        private val mView:View = iView
+        private val mGestureListener: GestureDetector.OnGestureListener = object : GestureDetector.SimpleOnGestureListener() {
+            private var mMotionDownX = 0f
+            private var mMotionDownY = 0f
+            override fun onDown(e: MotionEvent): Boolean {
+                mMotionDownX = e.rawX - mView.translationX
+                mMotionDownY = e.rawY - mView.translationY
+                return true
+            }
+
+            override fun onSingleTapUp(e: MotionEvent): Boolean {
+                return true
+            }
+
+            override fun onScroll(e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+                mIsScrolling = true
+//                trash.setVisibility(View.VISIBLE)
+                mView.translationX = e2.rawX - mMotionDownX
+                mView.translationY = e2.rawY - mMotionDownY
+                return true
+            }
+        }
+
+        private var mGestureDetector: GestureDetector = GestureDetector(iView.context, mGestureListener)
+
+        override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+            if (event?.action == MotionEvent.ACTION_UP){
+                mIsScrolling = false
+            }
+
+            if(mGestureDetector.onTouchEvent(event)){
+                return true
+            }
+
+            if(event?.action == MotionEvent.ACTION_UP){
+                if(mIsScrolling){
+                    mIsScrolling = false
+                }
+            }
+            return false
+        }
     }
 
 }
